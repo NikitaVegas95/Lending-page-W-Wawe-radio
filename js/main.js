b94788771c2cc1381542e252deaabfae52c52216
NIKITA <nikitavegas95@gmail.com>
2022-08-16 16:16:53 +0300

    header - 320 (90%)

--- js/main.js
+++ js/main.js
@@ -27,30 +27,43 @@ const swiper = new Swiper('.swiper', {
 });
 
 
-let headerEnableNaw = document.querySelector('.btn-svg-enable-naw'),
-    headerDisableNaw = document.querySelector('.btn-svg-disable-naw'),
-    headerBtnNaw = document.querySelector('.header__btn-bottom_naw'),
-    headerEnableArchive = document.querySelector('.btn-svg-enable-archive'),
-    headerDisableArchive = document.querySelector('.btn-svg-disable-archive'),
-    headerBtnArchive = document.querySelector('.header__btn-bottom_archive'),
-    headerNavTop = document.querySelector('.header__nav-top'),
-    burger = document.querySelector('.burger'),
-    header = document.querySelector('.header'),
-    burgerLineMenuCloze = document.querySelector('.burger-line-menu');
+const headerEnableNaw = document.querySelector('.btn-svg-enable-naw'),
+      headerDisableNaw = document.querySelector('.btn-svg-disable-naw'),
+      headerBtnNaw = document.querySelector('.header__btn-bottom_naw'),
+      headerEnableArchive = document.querySelector('.btn-svg-enable-archive'),
+      headerDisableArchive = document.querySelector('.btn-svg-disable-archive'),
+      headerBtnArchive = document.querySelector('.header__btn-bottom_archive'),
+      headerNavTop = document.querySelector('.header__nav-top'),
+      burger = document.querySelector('.burger'),
+      header = document.querySelector('.header'),
+      burgerLineMenuClose = document.querySelector('.burger-line-menu');
 
 
-    headerBtnNaw.addEventListener('click', function () {
-      headerEnableNaw.classList.toggle('btn-svg-disable-naw');
-      headerDisableNaw.classList.toggle('btn-svg-enable-naw');
-    });
+      headerBtnNaw.addEventListener('click', function () {
+        headerEnableNaw.classList.toggle('btn-svg-disable-naw');
+        headerDisableNaw.classList.toggle('btn-svg-enable-naw');
+      });
 
-    headerBtnArchive.addEventListener('click', function () {
-      headerEnableArchive.classList.toggle('btn-svg-disable-archive');
-      headerDisableArchive.classList.toggle('btn-svg-enable-archive');
-    });
+      headerBtnArchive.addEventListener('click', function () {
+        headerEnableArchive.classList.toggle('btn-svg-disable-archive');
+        headerDisableArchive.classList.toggle('btn-svg-enable-archive');
+      });
 
-    burger.addEventListener('click', function () {
-      header.classList.toggle('header-active');
-      headerNavTop.classList.toggle('open');
-      burgerLineMenuCloze.classList.toggle('burger-line_close-menu')
-    });
+      burger.addEventListener('click', function () {
+        header.classList.toggle('header-active');
+        headerNavTop.classList.toggle('open');
+        burgerLineMenuClose.classList.toggle('burger-line_close-menu')
+      });
+
+const mediaQuery = window.matchMedia('(max-width: 560px)')
+
+      if (mediaQuery.matches) {
+        const burger = document.querySelector('.burger'),
+              headerNavBottom = document.querySelector('.header__nav-bottom')
+              headerBottom = document.querySelector('.header__bottom');
+
+        burger.addEventListener('click', function () {
+          headerNavBottom.classList.toggle('open-bottom');
+          headerBottom.classList.toggle('header__bottom_bg');
+        });
+      }
