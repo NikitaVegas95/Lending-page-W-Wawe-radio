ef6a65280e305e64179d99b8d2b071d36ae27534
NIKITA <nikitavegas95@gmail.com>
2022-09-07 11:18:31 +0300

    validation

--- js/just_validate.js
+++ js/just_validate.js
@@ -1,15 +1,8 @@
-//маска номера
-
-const selector = document.querySelector("input[type='tel']");
-const im = new Inputmask("+7 (999)-999-99-99");
-
-im.mask(selector);
-
 //валидатор
 
 const validate = new window.JustValidate('form', {
   errorLabelStyle: {
-    color: 'var(--btn-color)',
+    color: 'red',
   }
 });
 
@@ -26,19 +19,6 @@ validate.addField('#name', [
     {
       rule: 'required',
       errorMessage: 'Вы не ввели имя',
-    },
-  ])
-  .addField('#tel', [
-    {
-      rule: 'required',
-      errorMessage: 'Вы не ввели телефон',
-    },
-    {
-      validator: (value) => {
-        const phone = selector.inputmask.unmaskedvalue()
-        return Number(phone) && phone.length === 10
-      },
-      errorMessage: 'не менее 10-ти цифр',
     },
   ])
   .addField('#email', [
